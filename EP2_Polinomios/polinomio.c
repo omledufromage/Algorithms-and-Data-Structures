a7a5d72e65289f4aed4c24a75c2e233449adcf97
omledufromage <mreverbel@protonmail.com>
2023-10-29 16:42:41 +0100

    Updated polinomios.c

--- EP2_Polinomios/polinomio.c
+++ EP2_Polinomios/polinomio.c
@@ -2,42 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mallocc.h"
-#include "polinomio.h"
-#include "pilha.h"
 #include "item.h"
-
+#include "polinomio.h"
 
 static Termo *lista_livre = NULL;
 
-static void s_intercala(int p, int q, int r, int v[]) {
-    int i, j, *w;
-    w = malloc((r-p) * sizeof (int));
-
-    for (i = p; i < q; ++i) w[i-p] = v[i];
-    for (j = q; j < r; ++j) w[r-p+q-j-1] = v[j];
-    i = 0; j = r-p-1;
-    for (int k = p; k < r; ++k)
-        if (w[i] <= w[j]) v[k] = w[i++];
-        else v[k] = w[j--];
-    free (w);
-}
-
-static void intercala(int p, int q, int r, int v[]) {
-    int *w;  // 1
-    w = malloc((r-p) * sizeof (int));  // 2
-    int i = p, j = q;  // 3
-    int k = 0;  // 4
-
-    while (i < q && j < r) {  // 5
-        if (v[i] <= v[j])  w[k++] = v[i++];  // 6
-        else  w[k++] = v[j++];  // 7
-    }  // 8
-    while (i < q)  w[k++] = v[i++];  // 9
-    while (j < r)  w[k++] = v[j++];  // 10
-    for (i = p; i < r; ++i)  v[i] = w[i-p];  // 11
-    free (w);  // 12
-}
-
+/*
 static void intercala(Polinomio p, Polinomio q) {
     Termo *t, *w, *r;
 
@@ -58,18 +28,27 @@ static void intercala(Polinomio p, Polinomio q) {
         }
     }
 }
+*/
 static Termo *aloca_termo() {
-    Polinomio p;
+    Termo *reciclado;
 
     if (lista_livre == NULL)
-        p = mallocX(sizeof(Polinomio));
-    else
-        p = desempilha();
-    return
+        reciclado = mallocX(sizeof(Polinomio));
+    else {
+        if (lista_livre == NULL) {
+            fprintf(stderr, "Erro: pilha vazia!\n");
+            exit(EXIT_FAILURE);
+        }
+        reciclado = lista_livre;
+        lista_livre = lista_livre->next;
+    }
+    return reciclado;
 }
 
-static void libera_termo(Termo *p) {
+static void libera_termo(Termo *novo) {
 
+    novo->next = lista_livre;
+    lista_livre = novo;
 }
 
 Polinomio cria_monomio(double coef, int exp) {
@@ -136,5 +115,12 @@ void imprime(Polinomio p, FILE *arq) {
 }
 
 void libera(Polinomio p) {
+    Termo *i;
 
+    while (p->next != NULL) {
+        i = p;
+        p = p->next;
+        libera_termo(i);
+    }
 }
+
